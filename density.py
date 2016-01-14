f23b6ca07518d3d812076889b433f4fd412c8efe
Isaiah Zhige Xin <xinzhige8@gmail.com>
2016-01-14 08:20:37 -0800

    Create density.py

---
+++ density.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def density(similarity, indicator):
+    m = indicator.shape[0]
+    n = indicator.shape[1]
+    result = 0 
+    for j in range(n):
+        num_nodes = np.count_nonzero(indicator[:, j]) 
+	      nodes = indicator[:, j].nonzero()[0].tolist()
+	      num_edges = 0
+        for i in nodes:
+            for k in nodes:
+                if i < k and similarity[i, k].all():
+                    num_edges += 1
+	      denominator = ((num_nodes * (num_nodes - 1)) / 2 - (num_nodes - 1))
+	      if denominator != 0:
+	          tmp = (num_edges - (num_nodes - 1)) / denominator
+	      else:
+            tmp = 0
+        result += (tmp * num_edges) / n
+    return result
